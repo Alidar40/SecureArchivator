1013068ae91c40bc655888cb0833f4f7aa569fb9
Alidar Asvarov <alidar.asvarov@gmail.com>
2018-10-06 21:20:39 +0300

    Refactoring and improvements of packing algorithm

--- main.go
+++ main.go
@@ -12,15 +12,14 @@ import
 	"path/filepath"
 	pkcs7 "github.com/fullsailor/pkcs7"
 	"crypto/x509"
-	"crypto/rsa"
+	"crypto/tls"
     "crypto/sha1"
-    "encoding/pem"
     "encoding/json"
     "encoding/binary"
 )
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("Processing...\n")
 
 	var mode, hash, cert, pkey, path string
 
@@ -41,90 +40,95 @@ func main() {
 		"Pkey")
 
 	flag.StringVar(
-		&path, "path", "./", 
+		&path, "path", "./packme", 
 		"Path")
 
 	flag.Parse()
 
 	switch mode {
 	case "z": 
-		szip(path, cert, pkey)
+		err := szip(path, cert, pkey)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(-1)
+		}
+
+		fmt.Printf("Packed successfully!\n")
+		os.Exit(0)
 	case "x":
 		extract()
 	case "i":
 		info()
 	default:
 		fmt.Printf("Enter mode\n")
+		os.Exit(-1)
 	}
 
 }
 
-func szip (path string, cert string, pkey string) {
+func szip (path string, cert string, pkey string) error {
 	//Create buffer and writer for zip archive
 	zipBuf := new(bytes.Buffer)
 	zipWriter := zip.NewWriter(zipBuf)
 
-	//Create buffer and writer for metadata
-	metaBuf := new(bytes.Buffer)
-	metaBuf.WriteString("files: [ \n")
+	//Create buffer for metadata
+	var meta []FileMeta
 
 	//Zip files
-	ZipFileWriter(path, filepath.Base(path) + "/", zipWriter, metaBuf)
-
-	//Add closing brackets to metadata
-	metaBuf.WriteString("]")
-
-	//Create metadata file
-	metaFile, err := os.Create("meta.json")
+	err := ZipFileWriter(path, filepath.Base(path) + "/", zipWriter, &meta)
 	if err != nil {
-	    log.Fatal(err)
-	}
-	metaFile.Write(metaBuf.Bytes())
-	metaFile.Close()	
-
-	//Create COMPRESSED metadata 
-	zipMetaBuf := new(bytes.Buffer)
-	zipMetaWriter := zip.NewWriter(zipMetaBuf)
-	m, err := zipMetaWriter.Create("meta.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	m.Write(metaBuf.Bytes())
-	err = zipMetaWriter.Close()
-	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	zipMetaFile, err := os.Create("compressed_meta.zip")
-	if err != nil {
-	    log.Fatal(err)
-	}
-	zipMetaFile.Write(zipMetaBuf.Bytes())
-	zipMetaFile.Close()
-
-	// Closing zip writer and creating .zip file
+	//Closing zip writer 
+	//If defer was used, loss of data would occur
 	err = zipWriter.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	zipFile, err := os.Create(filepath.Base(path) + ".zip")
+	//Obtaining meta
+    jsonMeta, err := json.Marshal(&meta)
+    if err != nil {
+        return err
+    }
+
+	//Create compressed metadata 
+	zipMetaBuf := new(bytes.Buffer)
+	zipMetaWriter := zip.NewWriter(zipMetaBuf)
+	m, err := zipMetaWriter.Create("meta.json")
 	if err != nil {
-	    log.Fatal(err)
+		return err
 	}
-	zipFile.Write(zipBuf.Bytes())
-	zipFile.Close()
+
+	_, err = m.Write(jsonMeta)
+	if err != nil {
+		return err
+	}
+
+	//Closing zip writer for metadata
+	//If defer was used, loss of data would occur
+	err = zipMetaWriter.Close()
+	if err != nil {
+		return err
+	}
 
 	//Creating .szp file
 	metaSize := new(bytes.Buffer)
-	err = binary.Write(metaSize, binary.BigEndian, uint32(binary.Size(metaBuf.Bytes())))
+	err = binary.Write(metaSize, binary.BigEndian, uint32(binary.Size(jsonMeta)))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(binary.BigEndian.Uint32(metaSize.Bytes()))
+
 	stufToSign := append(metaSize.Bytes(), zipMetaBuf.Bytes()...)
 	stufToSign = append(stufToSign, zipBuf.Bytes()...)
-	SignArchive(stufToSign, filepath.Base(path) + ".szp", cert, pkey)
+
+	err = SignArchive(stufToSign, filepath.Base(path) + ".szp", cert, pkey)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func extract() {
@@ -155,12 +159,12 @@ func FileToMeta(header *zip.FileHeader, fileBody []byte) (FileMeta){
 	return fileMeta
 }
 
-func ZipFileWriter(path string, pathTrace string, zipWriter *zip.Writer, metaBuf *bytes.Buffer) {
+func ZipFileWriter(path string, pathTrace string, zipWriter *zip.Writer, meta *[]FileMeta) error {
 
     //Get all files from desired path
 	filesToWrite, err := ioutil.ReadDir(path)
     if err != nil {
-        log.Fatal(err)
+        return err
     }
 
 	//Create folder in current directory for unzip
@@ -169,89 +173,75 @@ func ZipFileWriter(path string, pathTrace string, zipWriter *zip.Writer, metaBuf
 	//Search all files inside this destination
 	for _, file := range filesToWrite {
 		if file.IsDir(){
-			ZipFileWriter(path + "/" + file.Name(), pathTrace + file.Name() + "/", zipWriter, metaBuf)
+			ZipFileWriter(path + "/" + file.Name(), pathTrace + file.Name() + "/", zipWriter, meta)
 		} else {
 			f, err := zipWriter.Create(pathTrace + file.Name())
 			if err != nil {
-	            log.Fatal(err)
+	            return err
 	        }
 
 	        fileBody, err := ioutil.ReadFile(path + "/" + file.Name())
 	        _, err = f.Write(fileBody)
 	        if err != nil {
-	            log.Fatal(err)
+	            return err
 	        }
 
 	        fileHeader, err := zip.FileInfoHeader(file)
 	        if err != nil {
-	            log.Fatal(err)
-	        }
-	        meta := FileToMeta(fileHeader, fileBody)
-
-	        jsonMeta, err := json.Marshal(&meta)
-	        if err != nil {
-	            log.Fatal(err)
+	            return err
 	        }
 
-	        metaBuf.WriteString(string(jsonMeta))
-	        metaBuf.WriteString(", \n")
+	        *meta = append(*meta, FileToMeta(fileHeader, fileBody))
 		}
 		
 	}
+
+	return nil
 }
 
-func SignArchive(stufToSign []byte, name string, cert string, pkey string){
+func SignArchive(stufToSign []byte, name string, cert string, pkey string) error {
 	//Create data to sign
 	signedData, err := pkcs7.NewSignedData(stufToSign)
 	if err != nil {
-	    fmt.Printf("Cannot initialize signed data: %s", err)
+	    return err
 	}
 
-	//Load certificate
-	certFile, err := ioutil.ReadFile(cert)
+	//Parse certificate file
+	certificate, err := tls.LoadX509KeyPair(cert, pkey)
 	if err != nil {
-		log.Fatal(err)
-	}
-	certBlock, _ := pem.Decode(certFile)
-	if certBlock == nil {
-		panic("failed to parse certificate PEM")
-	}
-	certificate, err := x509.ParseCertificate(certBlock.Bytes)
-	if err != nil {
-		panic("failed to parse certificate: " + err.Error())
+		return err
 	}
 
-	//Load private key
-	pkeyFile, err := ioutil.ReadFile(pkey)
+	//Obtain the necessary key
+	rsaPKey := certificate.PrivateKey
+	rsaCert, err := x509.ParseCertificate(certificate.Certificate[0])
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	block, _ := pem.Decode(pkeyFile)
-	parseResult, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-	var privateKey *rsa.PrivateKey
-	privateKey = parseResult.(*rsa.PrivateKey)
-	if err != nil {
-		fmt.Println("!!")
-		log.Fatal(err)
-	}
-
 
 	//Sign data
-	signedData.AddSigner(certificate, privateKey, pkcs7.SignerInfoConfig{}) 
+	signedData.AddSigner(rsaCert, rsaPKey, pkcs7.SignerInfoConfig{}) 
 	if err != nil {
-	    log.Fatal("Cannot add signer: %s", err)
+	    return err
 	}
 
+	//Obtain final bytes
 	szip, err := signedData.Finish()
 	if err != nil {
-	    log.Fatal("Cannot finish signing data: %s", err)
+	    return err
 	}
 
+	//Write them to file
 	szpFile, err := os.Create(name)
 	if err != nil {
-	    log.Fatal(err)
+	    return err
 	}
-	szpFile.Write(szip)
-
-	szpFile.Close()
+	defer szpFile.Close()
+	
+	_, err = szpFile.Write(szip)
+	if err != nil {
+		return err
+	}
+	
+	return nil
 }
